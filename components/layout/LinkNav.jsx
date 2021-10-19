4e0ce8c3b7b2d88da8655c8dc9aa9de4d19dc182
Jack Sari <janasarii@gmail.com>
2021-10-18 20:13:25 -0500

    add config tailwind

--- components/layout/LinkNav.jsx
+++ components/layout/LinkNav.jsx
@@ -4,7 +4,6 @@ import {useRouter} from "next/router";
 
 function LinkNav({href, text}) {
     const router = useRouter();
-    console.log(router.asPath)
     return (
         <Link href={href}>
             <a className={`text-white font-light mx-4 border-b-2 pb-1 ${href == router.asPath ? 'font-bold' : 'border-transparent'} hover:border-white active:border-white transition`}>{text}</a>
